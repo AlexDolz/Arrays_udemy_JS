9bb75a802424de05e611e5efb598583df2b7891e
AlexDolz <aleksejdolznikov3@gmail.com>
2023-07-12 11:07:05 +0200

    chaining methods

--- script.js
+++ script.js
@@ -175,3 +175,12 @@ const max2 = movements.reduce(
 );
 
 console.log(max2);
+
+// ***************************** Chaining methods **********************
+
+const eurToUsd = 1.1;
+const totalDepositsUSD = movements
+  .filter(mov => mov > 0)
+  .map(mov => mov * eurToUsd)
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(totalDepositsUSD);
