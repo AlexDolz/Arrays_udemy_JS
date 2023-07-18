106c4b76a3c66ea987f0a128c1c5e27ddd71bca7
AlexDolz <aleksejdolznikov3@gmail.com>
2023-07-18 12:27:23 +0200

    updated

--- script.js
+++ script.js
@@ -302,3 +302,105 @@ console.log(movements);
 // console.log(movements);
 movements.sort((a, b) => b - a);
 console.log(movements);
+
+// **************** fill method, filling arrays programmatically *******
+
+const array = [1, 2, 3, 4, 5, 6, 7];
+
+const x = new Array(7);
+console.log(x);
+
+// x.fill(1); // [1,1,1,1,1,1,1]
+// console.log(x);
+
+// x.fill(1, 3); // [3 empty, 1, 1, 1, 1]
+// console.log(x);
+
+// x.fill(1, 3, 5); // [3 empty, 1, 1, 2 empty]
+// console.log(x);
+
+// array.fill(23, 2, 6); // [1,2,23,23,23,23,7]
+// console.log(array);
+
+// Array.from
+// const y = Array.from({ length: 7 }, () => 1);
+// console.log(y); // [1,1,1,1,1,1,1]
+
+// const z = Array.from({ length: 7 }, (_, i) => i + 1);
+// console.log(z); // [1,2,3,4,5,6,7]
+
+// const randomDice = Array.from({ length: 100 }, () =>
+//   Math.floor(Math.random() * 10)
+// );
+// console.log(randomDice);
+
+labelBalance.addEventListener('click', () => {
+  const movementsUI = Array.from(
+    document.querySelectorAll('.movements__value'),
+    el => Number(el.textContent.replace('€', ''))
+  );
+});
+
+// 2 solution, and than map separate
+const movementsUI2 = [...document.querySelectorAll('.movements__value')];
+
+// *********************** Arrays methods practice *******************
+// ----------------1.
+// const bankDepositSum = accounts.map(acc => acc.movements).flat();
+// const bankDepositSum = accounts
+//   .flatMap(acc => acc.movements)
+//   .filter(mov => mov > 0)
+//   .reduce((sum, cur) => sum + cur, 0);
+// console.log(bankDepositSum);
+
+// ----------------2.
+// const numDeposit1000 = accounts
+//   .flatMap(acc => acc.movements)
+//   .filter(mov => mov >= 1000).length;
+// console.log(numDeposit1000);
+
+// const numDeposit1000 = accounts
+//   .flatMap(acc => acc.movements)
+//   .reduce((count, cur) => (cur >= 1000 ? ++count : count), 0);
+// console.log(numDeposit1000);
+
+// -----------------3.
+// const sums = accounts
+//   .flatMap(acc => acc.movements)
+//   .reduce(
+//     (sums, cur) => {
+//       cur > 0 ? (sums.deposits += cur) : (sums.withdrawals += cur);
+//       return sums;
+//     },
+//     { deposits: 0, withdrawals: 0 }
+//   );
+// console.log(sums);
+
+// 2 solution
+// const { deposits, withdrawals } = accounts
+//   .flatMap(acc => acc.movements)
+//   .reduce(
+//     (sums, cur) => {
+//       sums[cur > 0 ? 'deposits' : 'withdrawals'] += cur;
+//       return sums;
+//     },
+//     { deposits: 0, withdrawals: 0 }
+//   );
+// console.log(deposits, withdrawals);
+
+// ------------------4.
+// this is a nice title -> This Is a Nice Title
+// const convertTitleCase = title => {
+//   const capitalize = str => str[0].toUpperCase() + str.slice(1);
+//   const exceptions = ['a', 'an', 'the', 'but', 'or', 'on', 'in', 'and', 'with'];
+
+//   const titleCase = title
+//     .toLowerCase()
+//     .split(' ')
+//     .map(word => (exceptions.includes(word) ? word : capitalize(word)))
+//     .join(' ');
+//   return capitalize(titleCase);
+// };
+// console.log(convertTitleCase('this is a nice title'));
+// console.log(convertTitleCase('this is a LONG title but not too long'));
+// console.log(convertTitleCase('and here is another title with an EXAMPLE'));
